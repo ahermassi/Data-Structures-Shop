fae561e5a92d3b89801c6213de4ea6c1f8cdb521
Anouer Hermassi <ahermass@stevens.edu>
2019-12-10 14:17:43 -0500

    Graph initialization update

--- GraphAlgorithmDesignManual/Graph.py
+++ GraphAlgorithmDesignManual/Graph.py
@@ -7,12 +7,12 @@ class Edge:
 
 
 class Graph:
-    def __init__(self):
-        self.edges = []  # adjacency info, array of edges; edges[i] is a linked list representing the adjacent edges
-        # of vertex i
-        self.degree = []  # out-degree of each vertex
+    def __init__(self, directed):
+        self.edges = [None] * self.num_vertices  # adjacency info, array of edges; edges[i] is a linked list
+        # representing the adjacent edges of vertex i
+        self.degree = [0] * self.num_vertices  # out-degree of each vertex
         self.num_vertices = 0  # number of vertices in graph
         self.num_edges = 0  # number of edges in graph
-        self.directed = False  # is the graph directed?
+        self.directed = directed  # is the graph directed?
 
 
