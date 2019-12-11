a0e321e4460e62890cacc9e63be567de4846f570
Anouer Hermassi <ahermass@stevens.edu>
2019-12-11 13:47:19 -0500

    DFS update

--- GraphAlgorithmDesignManual/DFS.py
+++ GraphAlgorithmDesignManual/DFS.py
@@ -2,17 +2,24 @@ from GraphAlgorithmDesignManual.Graph import build_graph
 
 
 visited = set()
+processed = set()
 
 
 def dfs(graph, source):
     visited.add(source)
-    print(source)
+    # process_vertex_early(source)
     adjacent = graph.edges[source]
     while adjacent:
         val = adjacent.val
         if val not in visited:
+            # process_edge(source, val)
             dfs(graph, val)
+        elif val not in processed:
+            pass
+            # process_edge(source, val)
         adjacent = adjacent.next
+    # process_vertex_late(source)
+    processed.add(source)
 
 
 if __name__ == '__main__':
