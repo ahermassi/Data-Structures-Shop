6f9c465464d2c94875c3c116efe8675aa85a61a5
Anouer Hermassi <ahermass@stevens.edu>
2019-12-11 09:36:17 -0500

    Inserting edge and printing graph

--- GraphAlgorithmDesignManual/Graph.py
+++ GraphAlgorithmDesignManual/Graph.py
@@ -7,12 +7,52 @@ class Edge:
 
 
 class Graph:
-    def __init__(self, directed):
+    def __init__(self, num_vertices, num_edges, directed):
+        self.num_vertices = num_vertices  # number of vertices in graph
+        self.num_edges = num_edges  # number of edges in graph
         self.edges = [None] * self.num_vertices  # adjacency info, array of edges; edges[i] is a linked list
         # representing the adjacent edges of vertex i
         self.degree = [0] * self.num_vertices  # out-degree of each vertex
-        self.num_vertices = 0  # number of vertices in graph
-        self.num_edges = 0  # number of edges in graph
         self.directed = directed  # is the graph directed?
+
+
+def build_graph(directed):
+    num_vertices = int(input('Number of vertices: '))
+    num_edges = int(input('Number of edges: '))
+    graph = Graph(num_vertices, num_edges, directed)
+    for i in range(num_edges):
+        source = int(input('Source: '))
+        dest = int(input('Destination: '))
+        insert_edge(graph, source, dest, directed)
+    return graph
+
+
+def insert_edge(graph, source, dest, directed):
+    edge = Edge()
+    edge.val = dest
+    edge.next = graph.edges[source]
+    graph.edges[source] = edge
+    graph.degree[source] += 1
+    if not directed:
+        insert_edge(graph, dest, source, True)
+    else:
+        graph.num_edges += 1
+
+
+def print_graph(graph):
+    for i in range(graph.num_vertices):
+        print('Vertex', i)
+        adjacent = graph.edges[i]
+        while adjacent:
+            print('Edge ' + str(i) + '->' + str(adjacent.val))
+            adjacent = adjacent.next
+        print()
+
+
+if __name__ == '__main__':
+    graph = build_graph(False)
+    print_graph(graph)
+
+
 
 
