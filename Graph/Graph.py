0fc1df3a3bc10a2d86270449ade75e45c74d491a
Anouer Hermassi <ahermass@stevens.edu>
2019-12-12 11:00:16 -0500

    Dictionary based graph

---
+++ Graph/Graph.py
@@ -0,0 +1,61 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, num_vertices, directed):
+        self.num_vertices = num_vertices  # number of vertices in graph
+        self.graph = defaultdict(list)  # adjacency info, hash map of vertices; graph[i] is a list representing the 
+        # adjacent vertices of vertex i
+        self.degree = defaultdict(int)  # out-degree of each vertex
+        self.directed = directed  # is the graph directed?
+
+    # a -> c
+    # b -> c
+    # b -> e
+    # c -> a
+    # c -> b
+    # c -> d
+    # c -> e
+    # d -> c
+    # e -> c
+    # e -> b
+
+    # The graph can be represented using the following dictionary:
+    # graph = { "a" : ["c"],
+    #           "b" : ["c", "e"],
+    #           "c" : ["a", "b", "d", "e"],
+    #           "d" : ["c"],
+    #           "e" : ["c", "b"],
+    #           "f" : []
+    #         }
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+        self.degree[u] += 1
+        if not self.directed:
+            self.graph[v].append(u)
+            self.degree[v] += 1
+            
+    def generate_edges(self):
+        edges = []
+        for vertex, adj_list in self.graph.items():
+            for adj in adj_list:
+                edges.append((vertex, adj))
+        return edges
+
+
+if __name__ == '__main__':
+    graph = Graph(5, True)
+    graph.add_edge('a', 'c')
+    graph.add_edge('b', 'c')
+    graph.add_edge('b', 'e')
+    graph.add_edge('c', 'd')
+    graph.add_edge('c', 'e')
+    graph.add_edge('c', 'a')
+    graph.add_edge('c', 'b')
+    graph.add_edge('e', 'b')
+    graph.add_edge('d', 'c')
+    graph.add_edge('e', 'c')
+    edges = graph.generate_edges()
+    for source, dest in edges:
+        print('{} -> {}'.format(source, dest))
