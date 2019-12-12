1a5bd3ceffc06997f4a03019fedd03e44f89cdc9
Anouer Hermassi <ahermass@stevens.edu>
2019-12-12 11:57:18 -0500

    Implementing Breadth First Search

--- Graph/BFS.py
+++ Graph/BFS.py
@@ -1,38 +1,30 @@
-from collections import defaultdict
-# The implementation uses adjacency list representation of graph For an illustrated procedure, check out this link:
-# https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
+from collections import deque
+from Graph import Graph
 
 
-class Graph:
-    def __init__(self):
-        self.graph = defaultdict(list)
-
-    def add_edge(self, u, v):
-        self.graph[u].append(v)
-
-    def bfs(self, source):
-        """ Traverse vertices reachable from source """
-        visited = [False] * len(self.graph)  # Mark all the vertices as not visited
-        queue = []
-        queue.append(source)
-        visited[source] = True
-        while len(queue) > 0:
-            temp = queue.pop(0)
-            print(temp, end=' ')
-            # Get all adjacent vertices of the dequeued vertex s. If a adjacent has not been visited, then mark it
-            # visited and enqueue it
-            for i in self.graph[temp]:
-                if not visited[i]:
-                    queue.append(i)
-                    visited[i] = True
+def bfs(source):
+    """ Traverse vertices reachable from source in a breadth-first manner. """
+    queue = deque([source])
+    visited[source] = True
+    while queue:
+        node = queue.popleft()
+        print(node, end=' ')
+        neighbors = g[source]
+        for neighbor in neighbors:
+            if not visited[neighbor]:
+                queue.append(neighbor)
+                visited[neighbor] = True
 
 
 if __name__ == '__main__':
-    graph = Graph()
+    graph = Graph(4, False)
     graph.add_edge(0, 1)
     graph.add_edge(0, 2)
     graph.add_edge(1, 2)
     graph.add_edge(2, 0)
     graph.add_edge(2, 3)
     graph.add_edge(3, 3)
-    graph.bfs(2)
+    n = graph.num_vertices
+    g = graph.graph
+    visited = [False] * n
+    bfs(2)
--- Graph/ConnectedComponents.py
+++ Graph/ConnectedComponents.py
@@ -4,6 +4,29 @@ from Graph import Graph
 # algorithm to find strongly connected components.
 
 
+def create_graph():
+    graph = Graph(18, False)
+    graph.add_edge(0, 8)
+    graph.add_edge(1, 5)
+    graph.add_edge(2, 9)
+    graph.add_edge(3, 9)
+    graph.add_edge(4, 0)
+    graph.add_edge(5, 16)
+    graph.add_edge(5, 17)
+    graph.add_edge(6, 11)
+    graph.add_edge(7, 6)
+    graph.add_edge(8, 4)
+    graph.add_edge(8, 14)
+    graph.add_edge(9, 15)
+    graph.add_edge(11, 7)
+    graph.add_edge(13, 0)
+    graph.add_edge(14, 0)
+    graph.add_edge(14, 13)
+    graph.add_edge(15, 2)
+    graph.add_edge(15, 10)
+    return graph
+
+
 def find_components():
     """ Start a DFS at every node (except if it's already been visited) and mark all reachable nodes as being part of
         the same component.
@@ -25,31 +48,13 @@ def dfs(i):
 
 
 if __name__ == '__main__':
-    graph = Graph(18, False)
-    graph.add_edge(0, 8)
-    graph.add_edge(1, 5)
-    graph.add_edge(2, 9)
-    graph.add_edge(3, 9)
-    graph.add_edge(4, 0)
-    graph.add_edge(5, 16)
-    graph.add_edge(5, 17)
-    graph.add_edge(6, 11)
-    graph.add_edge(7, 6)
-    graph.add_edge(8, 4)
-    graph.add_edge(8, 14)
-    graph.add_edge(9, 15)
-    graph.add_edge(11, 7)
-    graph.add_edge(13, 0)
-    graph.add_edge(14, 0)
-    graph.add_edge(14, 13)
-    graph.add_edge(15, 2)
-    graph.add_edge(15, 10)
 
+    graph = create_graph()
     n = graph.num_vertices
     g = graph.graph  # Adjacency list representing the graph
     visited = [False] * n
     count = 0
-    components = [-1] * n
+    components = [-1] * n  # components[i] is the component number that vertex i is part of
     find_components()
     print(count)
     print(components)
