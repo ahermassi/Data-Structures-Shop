060716250a5b856162ed5d0cdae0cd2f9a00c2d6
Anouer Hermassi <ahermass@stevens.edu>
2019-12-12 16:00:28 -0500

    Finding the shortest path in a grid

--- Graph/GridShortestPath.py
+++ Graph/GridShortestPath.py
@@ -7,25 +7,27 @@ def find_path(source, dest):
     parent = [[-1 for _ in range(7)] for _ in range(5)]  # Keep track of previously visited node (parent) for each node
     while queue:
         x, y, count = queue.popleft()
+        if (x, y) == dest:
+            return reconstruct_path(source, dest, parent, count)
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         for d1, d2 in directions:
             xx, yy = x + d1, y + d2
-            if 0 <= xx < n and 0 <= yy < m and (xx, yy) not in visited and grid[xx][yy] == '.':
+            if 0 <= xx < n and 0 <= yy < m and (xx, yy) not in visited and grid[xx][yy] != '#':
                 visited.add((xx, yy))
                 queue.append((xx, yy, count + 1))
                 parent[xx][yy] = (x, y)
 
-    return reconstruct_path(source, dest, parent)
+    return None, 0
 
 
-def reconstruct_path(source, dest, parent):
+def reconstruct_path(source, dest, parent, count):
     """ Reconstruct path going backwards from end node. """
     path = []
     while dest != -1:
         path.append(dest)
         dest = parent[dest[0]][dest[1]]
     path.reverse()  # Reverse the order of the nodes so that the path starts at 'source' node and ends at 'dest' node
-    return path if path[0] == source else None  # If 'source' and 'dest' are connected, return the path
+    return (path, count) if path[0] == source else (None, 0)  # If 'source' and 'dest' are connected, return the path
 
 
 if __name__ == '__main__':
